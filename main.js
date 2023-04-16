852d522f7f1ed50fb8b4dfca5a8436b721d71247
mazenmahmoudadly <mazenmahmoudadly@gmail.com>
2023-04-16 15:18:24 +0200

    init

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function save(){
+	console.log(localStorage.setItem('fname',document.getElementById('fname','mazen')));
+	console.log(localStorage.setItem('fname',document.getElementById('lname','mahmoud')));
+	console.log(localStorage.setItem('id',document.getElementById('studentID','20210318')));
+	console.log(localStorage.setItem('gpa',document.getElementById('GPA','3.266')));
+	console.log(localStorage.setItem('status',document.getElementById('Status')));
+	console.log(localStorage.setItem('email',document.getElementById('email')));
+	console.log(localStorage.setItem('date',document.getElementById('date')));
+	console.log(localStorage.setItem('gender',document.getElementById('Gender')));
+	console.log(localStorage.setItem('dept',document.getElementById('dept')));
+	console.log(localStorage.setItem('mobile',document.getElementById('mobile')));
+
+
+}
