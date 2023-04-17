f273cfe913778ea2cd17ae4c0763816d8b423e13
mohamed hesham <67235854+mohesham88@users.noreply.github.com>
2023-04-17 17:11:16 +0200

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -6,26 +6,3 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
-
-// validating input 
-const idInputContainer = document.querySelector(".id-input")
-const passwordContainer = document.querySelector(".password-input")
-const idInput = document.querySelector(".id-input input")
-const passwordInput = document.querySelector(".password-input input")
-
-
-let idWarningMsg = document.createElement("span")
-idWarningMsg.textContent = "* ID Must only contain numbers"
-idWarningMsg.classList.add("warning");
-
-
-// Dropdown list on mouse click functionality
-const studentsInfoLink = document.querySelector(".dropdown-list a")
-const dropdownMenu = document.querySelector(".dropdown-container")
-
-
-studentsInfoLink.addEventListener("click", ()=> {
-  dropdownMenu.classList.toggle("viewed")
-
-  console.log("clicked")
-})
