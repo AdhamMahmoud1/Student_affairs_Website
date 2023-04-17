bcee51fff65d9bb517792ad23e93ccdabae534ec
mohamed hesham <67235854+mohesham88@users.noreply.github.com>
2023-04-17 17:05:11 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -1,3 +1,28 @@
+
+// validating input 
+const idInputContainer = document.querySelector(".id-input")
+const passwordContainer = document.querySelector(".password-input")
+const idInput = document.querySelector(".id-input input")
+const passwordInput = document.querySelector(".password-input input")
+
+
+let idWarningMsg = document.createElement("span")
+idWarningMsg.textContent = "* ID Must only contain numbers"
+idWarningMsg.classList.add("warning");
+
+
+// Dropdown list on mouse click functionality
+const studentsInfoLink = document.querySelector(".dropdown-list a")
+const dropdownMenu = document.querySelector(".dropdown-container")
+
+
+studentsInfoLink.addEventListener("click", ()=> {
+  dropdownMenu.classList.toggle("viewed")
+
+  console.log("clicked")
+})
+
+
 // Used input event listener instead of change
 idInput.addEventListener("input", (e) => {
     if(isNaN(e.target.value) === false){
@@ -22,6 +47,35 @@ passwordInput.addEventListener("input", (e) => {
         passwordContainer.appendChild(passwordWarningMsg)
     }
 })
+
+// check ID and Password from local storage
+
+let admin = {
+  id : '20210543',
+  password : 'mohamedhesham123'
+}
+
+
+localStorage.setItem("admin",JSON.stringify(admin))
+
+let form = document.querySelector("form")
+
+form.addEventListener("submit",(e)=>{
+  e.preventDefault();
+  const idInput = document.querySelector(".id-input input").value
+  const passwordInput = document.querySelector(".password-input input").value
+  
+  let adminData = JSON.parse(localStorage.getItem("admin"))
+
+  if(idInput === adminData.id && passwordInput === adminData.password){
+    alert("Logged in successful")
+    const url = 'table.html';
+    window.location.href = url;
+  }else{
+    alert("invalid data")
+  }
+})
+
 
 
 
